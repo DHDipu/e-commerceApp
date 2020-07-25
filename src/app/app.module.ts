a9244ea03dc5c8193e625fc081ee95448300f22e
DHDipu <Dipuen2@gmail.com>
2020-07-25 22:36:28 +0600

    ng-bootstrap setup for dropdown menu working and all component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    LoginComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    MyOrdersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent
+      },
+      {
+        path: 'orders-success',
+        component: OrderSuccessComponent
+      },
+      {
+        path: 'my/orders',
+        component: MyOrdersComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent
+      },
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent
+      },
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
